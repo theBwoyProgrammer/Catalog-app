d8a6da3f2cd781536592572da6ea91796e6fe0dc
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-10-18 13:24:15 +0430

    Adding Method to the Item class

--- item.rb
+++ item.rb
@@ -1,11 +1,20 @@
 class Item
-    def initialize(id, genre, author, source, label, publish_date, archived)
-        @id = id
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize(id, label, publish_date, archived)
+    @id = id
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
 end
